922c77ab74ae700ead16f3235edfb7bc2663bda5
darkmodz427 <78821754+darkmodz427@users.noreply.github.com>
2021-02-11 00:48:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const moment = require('moment-timezone')
 const { exec } = require('child_process')
 const kagApi = require('@kagchi/kag-api')
 const fetch = require('node-fetch')
-const tiktod = require('tiktok-scraper')
+
 const ffmpeg = require('fluent-ffmpeg')
 const { removeBackgroundFromImageFile } = require('remove.bg')
 const imgbb = require('imgbb-uploader')
